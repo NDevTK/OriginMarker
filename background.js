b2615b129f80eca2c51290ec8e2668aa2a58a694
NDevTK <31563761+NDevTK@users.noreply.github.com>
2020-11-23 02:21:20 +0000

    Update background.js

--- background.js
+++ background.js
@@ -19,8 +19,7 @@ async function start() {
 }
 
 function onfocusChanged(e) {
-    if(e.windowId === -1) return
-    focused = e.windowId;
+    if(e.windowId === -1) focused = e.windowId;
     checkOrigin();
 }
 
