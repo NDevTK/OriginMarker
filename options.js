f730933d1f23ff4f7d49676137b7f03a66ba0e95
NDevTK <31563761+NDevTK@users.noreply.github.com>
2025-05-27 23:45:12 +0100

    Create options.js

---
+++ options.js
@@ -0,0 +1,9 @@
+var reset = document.getElementById('reset');
+
+reset.onclick = async () => {
+  if(!confirm('Are you sure?')) return
+  await chrome.storage.sync.clear();
+  await chrome.storage.local.clear();
+  await chrome.storage.session.clear();
+  alert('Any extension data has been removed.');
+}
