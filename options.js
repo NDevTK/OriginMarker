83e4e8b862212e3035e21d26d7c826bbf9cd818b
NDevTK <31563761+NDevTK@users.noreply.github.com>
2025-05-28 00:36:39 +0100

    Update options.js

--- options.js
+++ options.js
@@ -5,7 +5,5 @@ var reset = document.getElementById('reset');
 reset.onclick = async () => {
   reset.innerText = 'Clearing...';
   await chrome.storage.sync.clear();
-  await chrome.storage.local.clear();
-  await chrome.storage.session.clear();
   reset.innerText = 'Done but feel free to click again :)';
 };
