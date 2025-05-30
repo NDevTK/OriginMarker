c2e59a10f102b1c545ab4cead6a4bd92f5014728
NDevTK <31563761+NDevTK@users.noreply.github.com>
2025-05-30 21:20:39 +0100

    Update options.js

--- options.js
+++ options.js
@@ -1,9 +1,42 @@
 'use strict';
 
 var reset = document.getElementById('reset');
+var store = document.getElementById('store');
 
 reset.onclick = async () => {
   reset.innerText = 'Clearing...';
   await chrome.storage.sync.clear();
   reset.innerText = 'Done but feel free to click again :)';
 };
+
+store.onchange = async () => {
+  await setDataLocal('store', store.value);
+}
+
+async function main() {
+  const storeValue = await getDataLocal('store');
+  if (storeValue) store.value = storeValue;
+}
+
+function setDataLocal(key, value) {
+  return new Promise((resolve) => {
+    chrome.storage.local.set(
+      {
+        [key]: value
+      },
+      function (result) {
+        resolve(result);
+      }
+    );
+  });
+}
+
+function getDataLocal(key) {
+  return new Promise((resolve) => {
+    chrome.storage.local.get(key, function (result) {
+      resolve(result[key]);
+    });
+  });
+}
+
+main();
